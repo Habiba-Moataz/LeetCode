e2c9b98d12a85b49b4bb4c32a11c46193d9ce43b
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2025-05-10 22:30:34 +0300

    Create Reverse Integer.java

---
+++ Java/Reverse Integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int output =0 ,moduls,process = Math.abs(x);
+        int digits = (process == 0) ? 0 : (int) Math.log10(Math.abs(process));
+        while (process >= 10){
+            moduls = process%10 ;
+            output += moduls* Math.pow(10, digits);
+            process = process/10;
+            digits--;
+        }
+        output += process;
+        if (x<0){
+            output = output*-1;
+        }
+        return output;
+    }
+}
