2cebc00e0863777e6d3acf640e545fa5902ec379
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2025-07-29 21:44:39 +0300

    Create Longest Consecutive Sequence.java

---
+++ Java/Longest Consecutive Sequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int consecutive = 1, finalConsecutive = 0;
+        Arrays.sort(nums);
+        if (nums.length == 0) {
+            return 0;
+        } else {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i + 1]) {
+                    continue;
+                } else if (nums[i] + 1 == nums[i + 1]) {
+                    consecutive++;
+                } else {
+                    finalConsecutive = Math.max(finalConsecutive, consecutive);
+                    consecutive = 1;
+                }
+            }
+            return Math.max(finalConsecutive, consecutive);
+        }
+    }
+}
