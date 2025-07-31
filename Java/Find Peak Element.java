135f91d63caad5b0d9140e82fd1d126223edf6cb
Habiba-Moataz <120806492+Habiba-Moataz@users.noreply.github.com>
2025-07-31 15:31:33 +0300

    Create Find Peak Element.java

---
+++ Java/Find Peak Element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max = Arrays.stream(nums).max().getAsInt();
+        for (int i =0;i<nums.length;i++){
+            if (nums[i]==max){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
